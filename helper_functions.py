a2cf13ff3764d1c89a6025309109f6c635dc67ee
wokwen <wokwen@unc.edu>
2024-02-15 01:58:04 -0500

    loaded data

---
+++ helper_functions.py
@@ -0,0 +1,12 @@
+import os
+import tarfile
+from six.moves import urllib
+
+def fetch_housing_data(housing_url, housing_path):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
